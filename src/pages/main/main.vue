e91e001e536857ce022d8f57619a4f5b9cccf66f
Demetrius <3205561979@qq.com>
2019-11-26 08:46:31 +0800

    add ads#

--- src/pages/main/main.vue
+++ src/pages/main/main.vue
@@ -1,33 +1,38 @@
 <template>
     <view class="content">
-        <view v-if="hasLogin" class="hello">
-            <view class="title">
-                您好 {{userName}}，您已成功登录。
-            </view>
-            <view class="ul">
-                <view>这是 uni-app 带登录模板的示例App首页。</view>
-                <view>在 “我的” 中点击 “退出” 可以 “注销当前账户”</view>
-            </view>
-        </view>
-        <view v-if="!hasLogin" class="hello">
-            <view class="title">
-                您好 游客。
-            </view>
-            <view class="ul">
-                <view>这是 uni-app 带登录模板的示例App首页。</view>
-                <view>在 “我的” 中点击 “登录” 可以 “登录您的账户”</view>
-            </view>
-        </view>
+        <!--        <view v-if="hasLogin" class="hello">-->
+        <!--            <view class="title">-->
+        <!--                您好 {{userName}}，您已成功登录。-->
+        <!--            </view>-->
+        <!--            <view class="ul">-->
+        <!--                <view>这是 uni-app 带登录模板的示例App首页。</view>-->
+        <!--                <view>在 “我的” 中点击 “退出” 可以 “注销当前账户”</view>-->
+        <!--            </view>-->
+        <!--        </view>-->
+        <!--        <view v-if="!hasLogin" class="hello">-->
+        <!--            <view class="title">-->
+        <!--                您好 游客。-->
+        <!--            </view>-->
+        <!--            <view class="ul">-->
+        <!--                <view>这是 uni-app 带登录模板的示例App首页。</view>-->
+        <!--                <view>在 “我的” 中点击 “登录” 可以 “登录您的账户”</view>-->
+        <!--            </view>-->
+        <!--        </view>-->
+        <Ads></Ads>
     </view>
 </template>
 
 <script>
+    import Ads from '../../pages/publicDisplay/ads'
     import {
         mapState
     } from 'vuex'
 
     export default {
         computed: mapState(['forcedLogin', 'hasLogin', 'userName']),
+        components: {
+            Ads
+        },
         onLoad() {
             if (!this.hasLogin) {
                 uni.showModal({
@@ -39,9 +44,9 @@
                     showCancel: !this.forcedLogin,
                     success: (res) => {
                         if (res.confirm) {
-							/**
-							 * 如果需要强制登录，使用reLaunch方式
-							 */
+                            /**
+                             * 如果需要强制登录，使用reLaunch方式
+                             */
                             if (this.forcedLogin) {
                                 uni.reLaunch({
                                     url: '../login/login'
@@ -77,7 +82,7 @@
         margin-top: 25px;
     }
 
-    .ul>view {
+    .ul > view {
         line-height: 25px;
     }
 </style>
