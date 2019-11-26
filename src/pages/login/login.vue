b0f8a047a0c244a5a485c77c6851e5b0791bd02e
Demetrius <3205561979@qq.com>
2019-11-26 17:37:35 +0800

    修改登陆页面样式

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,26 +1,29 @@
 <template>
     <view class="content">
-        <view class="input-group">
-            <view class="input-row border">
-                <text class="title">账号：</text>
-                <m-input class="m-input" type="text" clearable focus v-model="account" placeholder="请输入账号"></m-input>
+        <view>
+            <view class="input-group">
+                <view class="input-row border">
+                    <text class="title">账号：</text>
+                    <m-input class="m-input" type="text" clearable focus v-model="account"
+                             placeholder="请输入账号"></m-input>
+                </view>
+                <view class="input-row">
+                    <text class="title">密码：</text>
+                    <m-input type="password" displayable v-model="password" placeholder="请输入密码"></m-input>
+                </view>
             </view>
-            <view class="input-row">
-                <text class="title">密码：</text>
-                <m-input type="password" displayable v-model="password" placeholder="请输入密码"></m-input>
+            <view class="btn-row">
+                <button type="primary" class="primary" @tap="bindLogin">登录</button>
             </view>
-        </view>
-        <view class="btn-row">
-            <button type="primary" class="primary" @tap="bindLogin">登录</button>
-        </view>
-        <view class="action-row">
-            <navigator url="../reg/reg">注册账号</navigator>
-            <text>|</text>
-            <navigator url="../pwd/pwd">忘记密码</navigator>
-        </view>
-        <view class="oauth-row" v-if="hasProvider" v-bind:style="{top: positionTop + 'px'}">
-            <view class="oauth-image" v-for="provider in providerList" :key="provider.value">
-                <image :src="provider.image" @tap="oauth(provider.value)"></image>
+            <view class="action-row">
+                <navigator url="../reg/reg">注册账号</navigator>
+                <text>|</text>
+                <navigator url="../pwd/pwd">忘记密码</navigator>
+            </view>
+            <view class="oauth-row" v-if="hasProvider" v-bind:style="{top: positionTop + 'px'}">
+                <view class="oauth-image" v-for="provider in providerList" :key="provider.value">
+                    <image :src="provider.image" @tap="oauth(provider.value)"></image>
+                </view>
             </view>
         </view>
     </view>
@@ -166,6 +169,13 @@
     .content {
         background: url("../../static/img/wallhaven-0pzq7m.jpg") no-repeat;
         background-size: cover;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .content > view > .input-group > .input-row {
+        padding: .5rem 0
     }
 
     .action-row {
@@ -174,8 +184,12 @@
         justify-content: center;
     }
 
+    .action-row > text {
+        color: #fff
+    }
+
     .action-row navigator {
-        color: #666;
+        color: #fff;
         padding: 0 10px;
     }
 
--- src/pages/publicDisplay/ads.vue
+++ src/pages/publicDisplay/ads.vue
@@ -3,48 +3,16 @@
         <view class="uni-padding-wrap">
             <view class="page-section swiper">
                 <view class="page-section-spacing">
-                    <swiper  class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval"
+                    <swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval"
                             :duration="duration">
                         <swiper-item v-for="item in adLinks" :key="item.id">
                             <view class="swiper-item uni-bg-red">
                                 <img :src="item.url" :width="item.width" :height="item.height" :alt="item.alt">
                             </view>
                         </swiper-item>
-<!--                        <swiper-item>-->
-<!--                            <view class="swiper-item uni-bg-green">-->
-<!--                                <img src="../../static/img/wallhaven-0pzq7m.jpg" width="100%" height="20%" alt="">-->
-<!--                            </view>-->
-<!--                        </swiper-item>-->
-<!--                        <swiper-item>-->
-<!--                            <view class="swiper-item uni-bg-blue">-->
-<!--                                <img src="../../static/img/wallhaven-0pzq7m.jpg" width="100%" height="20%" alt="">-->
-<!--                            </view>-->
-<!--                        </swiper-item>-->
                     </swiper>
                 </view>
             </view>
-        </view>
-        <view class="swiper-list">
-            <view class="uni-list-cell uni-list-cell-pd">
-                <view class="uni-list-cell-db">指示点</view>
-                <switch :checked="indicatorDots" @change="changeIndicatorDots"/>
-            </view>
-            <view class="uni-list-cell uni-list-cell-pd">
-                <view class="uni-list-cell-db">自动播放</view>
-                <switch :checked="autoplay" @change="changeAutoplay"/>
-            </view>
-        </view>
-        <view class="uni-padding-wrap">
-            <view class="uni-common-mt">
-                <text>幻灯片切换时长(ms)</text>
-                <text class="info">{{duration}}</text>
-            </view>
-            <slider @change="durationChange" :value="duration" min="500" max="2000"/>
-            <view class="uni-common-mt">
-                <text>自动播放间隔时长(ms)</text>
-                <text class="info">{{interval}}</text>
-            </view>
-            <slider @change="intervalChange" :value="interval" min="2000" max="10000"/>
         </view>
     </view>
 </template>
@@ -56,19 +24,19 @@
             return {
                 adLinks: [
                     {
-                        id:1,
+                        id: 1,
                         width: '100%',
                         height: '20%',
                         url: 'http://www.swjswang.com/uploads/allimg/161216/8-1612160T510F4.jpg',
                         alt: '广告图1'
                     }, {
-                        id:2,
+                        id: 2,
                         width: '100%',
                         height: '20%',
                         url: 'http://www.swjswang.com/uploads/170410/1-1F410232504D5.jpg',
                         alt: '广告图2'
                     }, {
-                        id:3,
+                        id: 3,
                         width: '100%',
                         height: '20%',
                         url: 'http://www.swjswang.com/uploads/allimg/170419/7-1F4191G02A36.jpg',
